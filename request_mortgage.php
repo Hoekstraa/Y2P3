f32b36307f6b14d798f1a082bdbb4a71af1af9cc
Sang Nguyen <sang.nguyen@student.nhlstenden.com>
2020-03-24 21:34:10 +0100

    Added name validation

--- request_mortgage.php
+++ request_mortgage.php
@@ -16,36 +16,36 @@ $navigation = [
 ];
 
 echo '<html lang="nl">';
-echo '<script type="text/javascript" src="nameValidation.js"></script>';
 
 	include("modular/head.php");
-	echo "<body>";
+	echo "<body onload=\"emailValidation(); firstNameValidation(); lastNameValidation()\">";
 		include("modular/navbar.php");
-		include("modular/header.php");
+        include("modular/header.php");
+		echo "<script src=\"nameValidation.js\"></script>";
 		echo "
 			<main>
 			<div class=\"mortgage-request\">
-				<h1>Hypotheek aanvragen</h1>
-				<p>Vraag hier uw hypotheek aan.</p>
+                <h1>Hypotheek aanvragen</h1>
+                <p id=\"head\">Vraag hier uw hypotheek aan.</p>
 				<div class=\"login-box\">
 					<form method=\"post\">
 						<label for=\"firstname\">Voornaam</label><br>
-						<input type=\"text\" class=\"firstname\" name=\"firstname\"></input>
+						<input type=\"text\" id=\"firstname\" name=\"firstname\"></input>
 						<br><br>
 						<label for=\"lastname\">Achternaam</label><br>
-						<input type=\"text\" class=\"lastname\"name=\"lastname\"></input>
+						<input type=\"text\" id=\"lastname\"name=\"lastname\"></input>
 						<br><br>
 						<label for=\"address\">Adres</label><br>
-						<input type=\"text\" name=\"address\"></input>
+						<input type=\"text\" id=\"address\"></input>
 						<br><br>
 						<label for=\"postalcode\">Postcode</label><br>
-						<input type=\"text\" name=\"postalcode\"></input>
+						<input type=\"text\" id=\"postalcode\"></input>
                         <br><br>
                         <label for=\"phone-number\">Telefoonnummer</label><br>
-						<input type=\"text\" name=\"phone-number\"></input>
+						<input type=\"text\" id=\"phone-number\"></input>
                         <br><br>
                         <label for=\"email\">Emailadres</label><br>
-						<input type=\"email\" name=\"email\"></input>
+						<input type=\"email\" id=\"email\" name=\email\></input>
 						<br><br>
 						<input class=\"submit\" type=\"submit\" value=\"Hypotheek aanvragen\"></input>
 					</form>
