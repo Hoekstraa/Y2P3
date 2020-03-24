712a6ed445ae9f88f0a3180baa01d74d3292792d
Thimo <tsdezwart@protonmail.com>
2020-03-24 13:47:18 +0100

    Add simple login prompt

--- login.php
+++ login.php
@@ -4,8 +4,8 @@ require "classes/NavbarItem.php";
 $title = "Home";
 $navigation = [
 	new NavbarItem("Hello", "index.php", false),
-	new NavbarItem("Bye", "bye.php", true),
-	new NavbarItem("Login", "login.php", false),
+	new NavbarItem("Bye", "bye.php", false),
+	new NavbarItem("Login", "login.php", true),
 	new NavbarItem("Register", "register.php", false),
 	//"test"
 ];
@@ -15,6 +15,25 @@ echo '<html lang="nl">';
 	echo "<body>";
 		include("modular/navbar.php");
 		include("modular/header.php");
+		echo "
+			<main>
+			<div class=\"login\">
+				<h1>Login</h1>
+				<p>Voer uw gegevens in om toegang te verkrijgen tot uw account.</p>
+				<div class=\"login-box\">
+					<form method=\"post\">
+						<label for=\"username\">Gebruikersnaam</label><br>
+						<input type=\"text\" name=\"username\"></input>
+						<br><br>
+						<label for=\"password\">Wachtwoord</label><br>
+						<input type=\"password\" name=\"password\"></input>
+						<br><br>
+						<input class=\"submit\" type=\"submit\" value=\"Login\"></input>
+					</form>
+				</div>
+			</div>
+		</main>
+		";
 		include("modular/footer.php");
 	echo "</body>";
 echo "</html>";
