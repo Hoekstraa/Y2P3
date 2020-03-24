f3694ed4bde17f911dae087b532b86b898068c80
Thimo <tsdezwart@protonmail.com>
2020-03-24 14:11:28 +0100

    Add register page

---
+++ register.php
@@ -0,0 +1,46 @@
+<?php
+require "classes/NavbarItem.php";
+
+$title = "Home";
+$navigation = [
+	new NavbarItem("Hello", "index.php", false),
+	new NavbarItem("Bye", "bye.php", false),
+	new NavbarItem("Login", "login.php", false),
+	new NavbarItem("Register", "register.php", true),
+	//"test"
+];
+
+echo '<html lang="nl">';
+	include("modular/head.php");
+	echo "<body>";
+		include("modular/navbar.php");
+		include("modular/header.php");
+		echo "
+			<main>
+			<div class=\"register\">
+				<h1>Registreren</h1>
+				<p>Registreer nu om toegang te krijgen tot de services van Ritsema Banken.</p>
+				<div class=\"login-box\">
+					<form method=\"post\">
+						<label for=\"username\">E-mailadres</label><br>
+						<input type=\"email\" name=\"email\"></input>
+						<br><br>
+						<label for=\"username\">Gebruikersnaam</label><br>
+						<input type=\"text\" name=\"email\"></input>
+						<br><br>
+						<label for=\"password\">Wachtwoord</label><br>
+						<input type=\"password\" name=\"password\"></input>
+						<br><br>
+						<label for=\"repeat-password\">Herhaal uw wachtwoord</label><br>
+						<input type=\"password\" name=\"repeat-password\"></input>
+						<br><br>
+						<input class=\"submit\" type=\"submit\" value=\"Login\"></input>
+					</form>
+				</div>
+			</div>
+		</main>
+		";
+		include("modular/footer.php");
+	echo "</body>";
+echo "</html>";
+?>
