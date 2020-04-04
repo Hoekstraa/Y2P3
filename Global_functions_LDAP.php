9af6f1aa11aedc9ebe27807d50fb27cd50f8c39f
Ork <horc44@gmail.com>
2020-04-04 22:28:50 +0200

    Test ldap_login

--- Global_functions_LDAP.php
+++ Global_functions_LDAP.php
@@ -4,8 +4,6 @@ function LDAPConnect()
 {
 	$LDAPServer = "localhost"; //todo goedzetten naar juiste domain
 	$ldap = ldap_connect($LDAPServer);
-	if($ldap) 
-	{ echo "Connected";}
 	return $ldap;
 }
 // LDAP LOGIN
@@ -14,33 +12,16 @@ function LDAPLogin($ldap)
 	$username ="cn=Manager,dc=ritsema,dc=frl";
 	ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
 	ldap_set_option($ldap, LDAP_OPT_REFERRALS, 0);
-	if($ldap)
-	{echo "connected2";}
 	$password = "secret";
-	$bind = ldap_bind($ldap,$username,$password);
-	if($bind) {
-		echo "bind";
-		$filter="(uid=$username)";
-		$result = ldap_search($ldap,"dc=ritsema,dc=frl",$filter);
-		ldap_sort($ldap,$result,"sn");
-		$info = ldap_get_entries($ldap, $result);
-		for ($i=0; $i<$info["count"]; $i++)
-		{
-			if($info['count'] > 1)
-			{
-				header("Location: Gekukt.php");
-				break;
-		}		
-			else 
-			{
-				$msg = "Invalid email address / password";
-				
-
-			}   
-
-		}
-	}
-else {echo "not bound";}
+    $bind = ldap_bind($ldap,$username,$password);
+    // Pull username from post request
+	$Uname = $_POST['username'];
+	// Pull Passwd from post request
+	$Passwd = $_POST['password'];
+    $attrs = ldap_get_attributes($ldap, "uid=$Uname,ou=Intranet,dc=ritsema,dc=frl");
+    for ($i=0; $i < $attrs["count"]; $i++) {
+        echo $attrs[$i] . "<br />";
+    }
 
 }
 
