dd3152760a39520f1ef6914ce672ad4a41c76afc
Thimo <tsdezwart@protonmail.com>
2020-04-05 15:48:38 +0200

    Fix spelling errors, Change javascript redirect to PHP

--- vendor/Project/Global_functions.php
+++ vendor/Project/Global_functions.php
@@ -249,13 +249,14 @@ Function CheckIfAdmin()
     // Check if the found variable is true
     if ($found == true) {
         // if found is true then pop up window with the text welkom
-        echo '<script type="text/javascript">alert("Welkom");</script>';
+        echo '<script type="text/javascript">alert("U bent ingelogd als administrator.");</script>';
     } else {
         // if found isnt true then pop up window with the text U bent geen admin vraag dit aan bij een van onze beheerders and then redirect to index.php
         echo '<script type="text/javascript">
-		alert("U bent geen admin vraag dit aan bij een van onze beheerders.");
-		window.location.href = "index.php";
+		alert("Toegang geweigerd. U heeft geen administratortoegang tot de website.");
 		</script>';
+		header("Location: index.php");
+		
     }
 }
 
@@ -347,7 +348,7 @@ function GetUserID($Session_id_user)
     return $userid;
 }
 
-// This fubction compares the token fomr hidden field with session token
+// This function compares the token fomr hidden field with session token
 function CompareToken_Consultant($token_session, $userid)
 {
     // Get token 1 from post
